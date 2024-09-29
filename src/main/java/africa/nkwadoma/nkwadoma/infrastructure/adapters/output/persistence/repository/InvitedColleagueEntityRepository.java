d3a7172308257218b87f0693d9e5b804cb1334de
Divinegirl8 <iamoluchimercy@gmail.com>
2024-09-29 22:31:43 +0100

    deleted not necessary object

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/InvitedColleagueEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.repository;
-
-import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.InvitedColleagueEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvitedColleagueEntityRepository extends JpaRepository<InvitedColleagueEntity, String> {
-}
