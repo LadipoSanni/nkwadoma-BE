1714dc7053dd9bb59096ed1b756a03e9cfa0a0e8
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 <ned@workmail.com>
2024-10-07 11:20:36 +0100

    restructing the adapter and the service for loan product. added usecase and working on the controller

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/loan/LoanProductAdapter.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/loan/LoanProductAdapter.java
@@ -6,11 +6,14 @@ import africa.nkwadoma.nkwadoma.domain.model.loan.LoanProduct;
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.mapper.loan.LoanProductMapper;
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.loanEntity.LoanProductEntity;
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.repository.loan.LoanProductEntityRepository;
-import africa.nkwadoma.nkwadoma.infrastructure.exceptions.LoanProductException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static africa.nkwadoma.nkwadoma.domain.validation.LoanValidator.validateLoanProduct;
+import static africa.nkwadoma.nkwadoma.domain.validation.LoanValidator.validateLoanProductDetails;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,36 +22,12 @@ public class LoanProductAdapter implements LoanProductOutputPort {
     private final LoanProductEntityRepository loanProductEntityRepository;
     private final LoanProductMapper loanProductMapper;
     @Override
-    public LoanProduct createLoanProduct(LoanProduct loanProduct) throws MiddlException {
-        validateLoanProduct(loanProduct);
-        validateLoanProductDetails(loanProduct);
+    public LoanProduct save(LoanProduct loanProduct)  {
         LoanProductEntity loanProductEntity = loanProductMapper.mapLoanProductToEntity(loanProduct);
+        loanProductEntity.setCreatedAtDate(LocalDateTime.now());
         LoanProductEntity savedLoanProductEntity = loanProductEntityRepository.save(loanProductEntity);
         loanProduct.setId(savedLoanProductEntity.getId());
         return loanProduct;
     }
 
-    private void validateLoanProductDetails(LoanProduct loanProduct) throws MiddlException{
-        if (StringUtils.isEmpty(loanProduct.getName())
-                ||StringUtils.isEmpty(loanProduct.getMandate())
-                ||loanProduct.getSponsors() == null
-                ||loanProduct.getSponsors().isEmpty()
-                ||loanProduct.getLoanProductSize() == null
-//            ||loanProduct.getLoanProductSize().equals(0)
-                ||loanProduct.getObligorLoanLimit() == null
-//            ||loanProduct.getObligorLoanLimit().equals(0)
-                ||loanProduct.getInterestRate() < 0
-                ||loanProduct.getMoratorium() < 0
-                ||loanProduct.getTenor() < 0
-                ||loanProduct.getMinRepaymentAmount() == null
-                ||StringUtils.isEmpty(loanProduct.getTermsAndCondition())
-        ) {
-            log.error("Invalid or empty request details to create loan product {} ",loanProduct);
-            throw new LoanProductException("Invalid or empty request details to create loan product");
-        }
-    }
-
-    private void validateLoanProduct(LoanProduct loanProduct)throws MiddlException {
-        if (loanProduct == null) throw new LoanProductException("Invalid details provided");
-    }
 }
