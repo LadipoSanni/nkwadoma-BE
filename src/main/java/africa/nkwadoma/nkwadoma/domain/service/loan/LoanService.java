e7d3a3249a14c982d14fbf2ed7bc3b5715b33430
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 <ned@workmail.com>
2024-10-07 13:38:59 +0100

    created validation class and enum for constants and rest messages.

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/loan/LoanService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/loan/LoanService.java
@@ -4,17 +4,9 @@ import africa.nkwadoma.nkwadoma.application.ports.input.loan.CreateLoanProductUs
 import africa.nkwadoma.nkwadoma.application.ports.output.loan.LoanProductOutputPort;
 import africa.nkwadoma.nkwadoma.domain.exceptions.MiddlException;
 import africa.nkwadoma.nkwadoma.domain.model.loan.LoanProduct;
-import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.mapper.loan.LoanProductMapper;
-import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.loanEntity.LoanProductEntity;
-import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.repository.loan.LoanProductEntityRepository;
-import africa.nkwadoma.nkwadoma.infrastructure.exceptions.LoanProductException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 import static africa.nkwadoma.nkwadoma.domain.validation.LoanValidator.validateLoanProduct;
 import static africa.nkwadoma.nkwadoma.domain.validation.LoanValidator.validateLoanProductDetails;
