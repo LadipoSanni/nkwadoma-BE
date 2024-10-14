d9b33b0e0b1baf5333cfd5f549da385282f1ed1a
tade8 <tadeemmanuel08@gmail.com>
2024-10-14 13:45:46 +0100

    controller update

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/IdentityManagerController.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/IdentityManagerController.java
@@ -27,17 +27,14 @@ public class IdentityManagerController {
 
     @PostMapping("auth/login")
     public ResponseEntity<ApiResponse<?>> login(@RequestBody @Valid UserIdentityRequest userIdentityRequest) throws MeedlException {
-        try {
-            UserIdentity userIdentity = identityMapper.toIdentity(userIdentityRequest);
-            AccessTokenResponse tokenResponse = createUserUseCase.login(userIdentity);
-            return ResponseEntity.ok(ApiResponse.<AccessTokenResponse>builder().
-                    body(tokenResponse).message(ControllerConstant.RESPONSE_IS_SUCCESSFUL.getMessage()).
-                    statusCode(HttpStatus.OK.name()).build());
-        } catch (MeedlException e) {
-            return new ResponseEntity<>(new ApiResponse<>(INVALID_OPERATION, e.getMessage(),
-                    HttpStatus.BAD_REQUEST.toString()), HttpStatus.BAD_REQUEST);
-        }
+        UserIdentity userIdentity = identityMapper.toIdentity(userIdentityRequest);
+        AccessTokenResponse tokenResponse = createUserUseCase.login(userIdentity);
+        return ResponseEntity.ok(ApiResponse.<AccessTokenResponse>builder().
+                body(tokenResponse).message(ControllerConstant.RESPONSE_IS_SUCCESSFUL.getMessage()).
+                statusCode(HttpStatus.OK.name()).build());
+
     }
+
     @PostMapping("auth/invite-colleague")
     public ResponseEntity<ApiResponse<?>> inviteColleague(@RequestBody UserIdentityRequest userIdentityRequest) throws MeedlException {
             UserIdentity userIdentity = identityMapper.toIdentity(userIdentityRequest);
@@ -46,18 +43,13 @@ public class IdentityManagerController {
                     body(createdUserIdentity).message(ControllerConstant.RESPONSE_IS_SUCCESSFUL.getMessage()).
                     statusCode(HttpStatus.OK.name()).build());
     }
+
     @PostMapping("auth/password/create")
     public ResponseEntity<ApiResponse<?>> createPassword(@RequestBody @Valid PasswordCreateRequest passwordCreateRequest) throws MeedlException {
-        log.info("Password request: " + passwordCreateRequest);
-//        try {
-            UserIdentity userIdentity = identityMapper.toPasswordCreateRequest(passwordCreateRequest);
-            return ResponseEntity.ok(ApiResponse.<UserIdentity>builder().
-                    body(createUserUseCase.createPassword(userIdentity.getEmail(), userIdentity.getPassword())).
-                    message(ControllerConstant.PASSWORD_CREATED_SUCCESSFULLY.getMessage()).
-                    statusCode(HttpStatus.OK.name()).build());
-//        } catch (MeedlException e) {
-//            return new ResponseEntity<>(new ApiResponse<>(INVALID_OPERATION, e.getMessage(),
-//                    HttpStatus.BAD_REQUEST.toString()), HttpStatus.BAD_REQUEST);
-//        }
+        UserIdentity userIdentity = identityMapper.toPasswordCreateRequest(passwordCreateRequest);
+        return ResponseEntity.ok(ApiResponse.<UserIdentity>builder().
+                body(createUserUseCase.createPassword(userIdentity.getEmail(), userIdentity.getPassword())).
+                message(ControllerConstant.PASSWORD_CREATED_SUCCESSFULLY.getMessage()).
+                statusCode(HttpStatus.OK.name()).build());
     }
 }
