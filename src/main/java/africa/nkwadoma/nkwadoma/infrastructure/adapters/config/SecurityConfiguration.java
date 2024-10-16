5e1174d198ef816c581944393364fde6cba006ca
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 <ned@workmail.com>
2024-10-16 10:43:38 +0100

    removed the enum constant and added a string constant

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/SecurityConfiguration.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
                 csrf(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests(requests -> {
-            requests.requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll();
+            requests.requestMatchers(HttpMethod.POST, "/api/v1/auth/login", "/api/v1/auth/password/create").permitAll();
             requests.anyRequest().authenticated();
         });
 
