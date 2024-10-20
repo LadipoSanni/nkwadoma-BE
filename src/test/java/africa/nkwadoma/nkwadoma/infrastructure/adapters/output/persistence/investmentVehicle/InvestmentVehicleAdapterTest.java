c9b77f2f080eba4e67edcc5cd19290e7c3d57495
Adeshina Qudus <qudusa55@gmail.com>
2024-10-20 01:00:51 +0100

    view all investment vehicle

--- src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/investmentVehicle/InvestmentVehicleAdapterTest.java
+++ src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/investmentVehicle/InvestmentVehicleAdapterTest.java
@@ -17,10 +17,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Slf4j
+@SpringBootTest
 class InvestmentVehicleAdapterTest {
 
     @Autowired
