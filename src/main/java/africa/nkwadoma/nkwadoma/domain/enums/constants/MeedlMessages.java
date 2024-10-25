8c16a2f0bb0ccf249e6351a34e886f85462a30db
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 <ned@workmail.com>
2024-10-25 15:22:58 +0100

    capitalisation

--- src/main/java/africa/nkwadoma/nkwadoma/domain/enums/constants/MeedlMessages.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/enums/constants/MeedlMessages.java
@@ -4,20 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum MeedlMessages {
-    EMPTY_INPUT_FIELD_ERROR("field cannot be null or empty"),
+    EMPTY_INPUT_FIELD_ERROR("Field cannot be null or empty"),
     INVALID_REQUEST("Request cannot be null or empty"),
-    INVALID_EMAIL_ADDRESS("email address is not valid"),
+    INVALID_EMAIL_ADDRESS("Email address is not valid"),
     INVALID_INDUSTRY_OR_SERVICE_OFFERING("Industry or service offering cannot be empty"),
-    EMAIL_NOT_FOUND("email address cannot be found"),
-    EMAIL_ALREADY_EXISTS("email address already exist"),
+    EMAIL_NOT_FOUND("Email address cannot be found"),
+    EMAIL_ALREADY_EXISTS("Email address already exist"),
     EMAIL_INVITATION_SUBJECT("Invitation to Middl"),
-    ORGANIZATION_INVITATION_TEMPLATE("organization-invitation"),
-    COLLEAGUE_INVITATION_TEMPLATE("colleague-invitation"),
-    DOMAIN_EMAIL_DOES_NOT_MATCH("domain email does not match"),
+    ORGANIZATION_INVITATION_TEMPLATE("Organization-invitation"),
+    COLLEAGUE_INVITATION_TEMPLATE("Colleague-invitation"),
+    DOMAIN_EMAIL_DOES_NOT_MATCH("Domain email does not match"),
     EMAIL_INDEX("@"),
-    INVALID_CREATED_BY("created by id not valid"),
+    INVALID_CREATED_BY("Created by id not valid"),
     INVALID_OBJECT("Object not found"),
-    UUID_NOT_VALID("uuid not valid"), NON_EXISTING_CREATED_BY("Creator not found");
+    UUID_NOT_VALID("UUID not valid"), NON_EXISTING_CREATED_BY("Creator not found");
 
 
     private final String message;
