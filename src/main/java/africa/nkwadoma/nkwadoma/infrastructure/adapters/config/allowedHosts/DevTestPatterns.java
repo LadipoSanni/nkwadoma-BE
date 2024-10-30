235c2ec55f5a8f5d1f079408cf77c2194aa698b9
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 <ned@workmail.com>
2024-10-30 10:28:23 +0100

    worked on allowed hosts

---
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/allowedHosts/DevTestPatterns.java
@@ -0,0 +1,18 @@
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.config.allowedHosts;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+
+@Profile("dev")
+@Component
+public class DevTestPatterns implements AllowedHost{
+    @Override
+    public String[] getPatterns() {
+        return new String[]{
+                "http://localhost:3000",
+                "http://localhost:3000/",
+        };
+    }
+}
