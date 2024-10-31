0252d25b144c055588c651d75674fca3ae05f933
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 <ned@workmail.com>
2024-10-31 19:34:34 +0100

    worked on the mapper for duration type

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/mapper/DurationTypeMapper.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/mapper/DurationTypeMapper.java
@@ -6,16 +6,4 @@ import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface DurationTypeMapper {
-    @EnumMapping(nameTransformationStrategy = MappingConstants.CASE_TRANSFORMATION, configuration = "upper")
-    @ValueMappings({
-            @ValueMapping(target = "MONTHS", source = "Months"),
-            @ValueMapping(target = "WEEKS", source = "Weeks"),
-            @ValueMapping(target = "YEARS", source = "Years")
-    })
-    DurationType toDurationType(String durationType);
-
-    default DurationType maptoDurationType(String durationType) {
-        durationType = durationType.trim().toUpperCase();
-        return DurationType.valueOf(durationType);
-    }
 }
