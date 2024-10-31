35d83e69d68c8056e9d558ae81c854f860bfeeaa
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 <ned@workmail.com>
2024-10-31 09:34:54 +0100

    still have a failing test to fix but will make a pr for safety

---
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/loan/LoanProductVendorRepository.java
@@ -0,0 +1,9 @@
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.repository.loan;
+
+import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.loanEntity.LoanProductVendor;
+import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.loanEntity.VendorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanProductVendorRepository extends JpaRepository<LoanProductVendor,String> {
+    void deleteAllByVendorEntity(VendorEntity vendorEntity);
+}
