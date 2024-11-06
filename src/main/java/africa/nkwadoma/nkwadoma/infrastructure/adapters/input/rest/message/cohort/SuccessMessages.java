f1bd473a13d45f0cd76cde1368fbc32d00b364ee
tade8 <tadeemmanuel08@gmail.com>
2024-11-06 12:49:30 +0100

    changed success messages

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/message/cohort/SuccessMessages.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/message/cohort/SuccessMessages.java
@@ -3,6 +3,6 @@ package africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.message.coho
 public class SuccessMessages {
     public static final String COHORT_CREATED  = "Cohort Created";
     public static final String COHORT_VIEWED = "viewed";
-    public static final String COHORT_INVITED= "All members of this cohort has been invited successfully";
+    public static final String COHORT_INVITED= "All members of this cohort have been invited successfully";
     public static final String COHORT_EDITED_SUCCESSFULLY = "Cohort Edited Successfully";
  }
