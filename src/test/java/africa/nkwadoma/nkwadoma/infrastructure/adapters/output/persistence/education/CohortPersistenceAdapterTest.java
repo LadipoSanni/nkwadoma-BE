67752a5ab12ebceab6479b334ae6266eefff9b94
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 <ned@workmail.com>
2024-11-11 13:34:16 +0100

    resolved conflicts

--- src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/education/CohortPersistenceAdapterTest.java
+++ src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/education/CohortPersistenceAdapterTest.java
@@ -85,18 +85,12 @@ class CohortPersistenceAdapterTest {
     @BeforeAll
     void setUpOrg() {
         meedleUser = UserIdentity.builder()
-                .firstName("Ford 20").role(IdentityRole.valueOf("PORTFOLIO_MANAGER")).
-                lastName("Benson Ayo").email("yanakix1146@lineacr.com").createdBy("61fb3beb-f200-4b16-ac58-c28d737b546c").build();
-        UserIdentity userIdentity = UserIdentity.builder()
                 .firstName("Ford").role(IdentityRole.PORTFOLIO_MANAGER).
-                lastName("Benson").email("freddy102@example.com").createdBy("61fb3beb-f200-4b16-ac58-c28d737b546c").build();
+                lastName("Benson Ayo").email("yanakix11h46@lineacr.com").createdBy("61fb3beb-f200-4b16-ac58-c28d737b546c").build();
         employeeIdentity = OrganizationEmployeeIdentity.builder()
                 .meedlUser(meedleUser).build();
         organizationIdentity = OrganizationIdentity.builder().email("fordorganization12@example.com")
                 .name("Organization test").rcNumber("56767").serviceOfferings(
-                .meedlUser(userIdentity).build();
-        organizationIdentity = OrganizationIdentity.builder().email("fordorganization012@example.com")
-                .name("Organization09 Ford").rcNumber("7576").serviceOfferings(
                         List.of(ServiceOffering.builder().industry(Industry.EDUCATION).name(ServiceOfferingType.TRAINING.name()).build())).
                 phoneNumber("09084567832").organizationEmployees(List.of(employeeIdentity))
                 .build();
