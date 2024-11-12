277c343e43a6a25d07f9f03a38e6198a2a720f27
Tade Emmanuel <timothywonder01@gmail.com>
2024-11-12 16:28:06 +0100

    updated cors config

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/SecurityConfiguration.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/SecurityConfiguration.java
@@ -16,8 +16,7 @@ import org.springframework.security.web.*;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.*;
 
@@ -46,17 +45,27 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    @Bean
-    WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins(allowedHost.getPatterns())
-                    .allowedMethods(allowedHost.getMethods())
-                    .allowedHeaders("*")
-                    .exposedHeaders("*").allowCredentials(true);
-         }};
-    }
+//    @Bean
+//    WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurer() {
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry) {
+//            registry.addMapping("/**")
+//                    .allowedOrigins(allowedHost.getPatterns())
+//                    .allowedMethods(allowedHost.getMethods())
+//                    .allowedHeaders("*")
+//                    .exposedHeaders("*").allowCredentials(true);
+//         }};
+//    }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedHost.getPatterns()));
+        configuration.setAllowedMethods(Arrays.asList(allowedHost.getMethods()));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
