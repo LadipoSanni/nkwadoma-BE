bbde9ee988f914516155551a115e7b25de1cb2de
Tade Emmanuel <timothywonder01@gmail.com>
2024-11-12 15:39:51 +0100

    updated pattern for allowed origin

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/allowedHosts/DevTestPatterns.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/allowedHosts/DevTestPatterns.java
@@ -15,9 +15,9 @@ public class DevTestPatterns implements AllowedHost{
                 "http://localhost:3000/",
                 "http://localhost:3000/**",
                 "https://api-systest.learnspace.africa/",
-                "http://api-systest.learnspace.africa/swagger-ui/**",
                 "http://api-systest.learnspace.africa",
                 "http://api-systest.learnspace.africa/**",
+                "http://api-systest.learnspace.africa/swagger-ui/**"
         };
     }
 }
