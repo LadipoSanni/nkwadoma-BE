55b8725e5fef73e504c9a1b534d8565bead105ea
Ned <ned@workmail.com>
2024-11-18 20:11:59 +0100

    Update LoanOfferOutputPort.java

--- src/main/java/africa/nkwadoma/nkwadoma/application/ports/output/loan/LoanOfferOutputPort.java
+++ src/main/java/africa/nkwadoma/nkwadoma/application/ports/output/loan/LoanOfferOutputPort.java
@@ -4,6 +4,5 @@ import africa.nkwadoma.nkwadoma.domain.model.loan.LoanOffer;
 
 public interface LoanOfferOutputPort {
     LoanOffer save(LoanOffer loanOffer);
-
     LoanOffer findLoanOfferById(String loanOfferId);
 }
