48da9d3cee1952643a72bb9775f7ffced810102e
Ned <ned@workmail.com>
2024-11-30 16:13:38 +0100

    Update SuccessMessages.java

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/message/loan/SuccessMessages.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/message/loan/SuccessMessages.java
@@ -4,5 +4,5 @@ public class SuccessMessages {
     public static final String LOANEE_ADDED_TO_COHORT = "Loanee Added To Cohort";
     public static final String LOANEE_VIEWED = "Loanee Viewed Successfully";
     public static final String LOANEE_HAS_BEEN_REFERED = "Loanee Has Been Refered";
-    public static final String LOANEE_RETRIVED = "Loanee Retrived";
+    public static final String LOANEE_RETRIVED = "Loanee Retrieved";
 }
