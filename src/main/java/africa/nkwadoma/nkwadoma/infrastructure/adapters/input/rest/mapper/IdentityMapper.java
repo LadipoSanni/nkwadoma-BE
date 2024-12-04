f4129ba9500904c293b34133404280f7d1c731b3
Tade Emmanuel <timothywonder01@gmail.com>
2024-12-04 06:25:02 +0100

    password null validation

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/mapper/IdentityMapper.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/mapper/IdentityMapper.java
@@ -2,7 +2,6 @@ package africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.mapper;
 
 import africa.nkwadoma.nkwadoma.domain.model.identity.*;
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.data.request.identity.*;
-import jakarta.validation.*;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +9,10 @@ public interface IdentityMapper {
     @Mapping(source = "email", target = "email")
     @Mapping(source = "password", target = "password")
     UserIdentity toIdentity(UserIdentityRequest userIdentityRequest);
+
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "password", target = "password")
+    UserIdentity toUserIdentity(PasswordChangeRequest passwordChangeRequest);
 
     @Mapping(source = "token", target = "email")
     @Mapping(source = "password", target = "password")
