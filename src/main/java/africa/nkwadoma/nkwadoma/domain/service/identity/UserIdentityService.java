69f7c6e6092db2dc2b6c0c4a609632a8ed436fe2
Adeshina Qudus <qudusa55@gmail.com>
2024-12-09 14:36:59 +0100

    moving to another branch

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/identity/UserIdentityService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/identity/UserIdentityService.java
@@ -111,6 +111,7 @@ public class UserIdentityService implements CreateUserUseCase  {
         }
     }
     private void passwordPreviouslyCreated(String token) throws IdentityException {
+        log.info("checking if its previously created  {}",token);
         if (blackListedTokenAdapter.isPresent(token)){
             log.info("Password already created before. Method called more than once with the same token.");
             throw new IdentityException("Password already created. Try login or forgot password. Or contact the admin ");
@@ -118,6 +119,7 @@ public class UserIdentityService implements CreateUserUseCase  {
     }
     @Override
     public UserIdentity createPassword(String token, String password) throws MeedlException {
+        log.info("request got into service layer {}",password);
         passwordPreviouslyCreated(token);
         UserIdentity userIdentity = getUserIdentityFromToken(password, token);
         userIdentity = identityManagerOutPutPort.createPassword(userIdentity.getEmail(), password);
