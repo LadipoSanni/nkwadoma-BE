b26bee8834c8a89d0ed9462ca51190f6bb43ce1d
Tade Emmanuel <timothywonder01@gmail.com>
2024-12-09 18:50:10 +0100

    resolved conflicts

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/identity/UserIdentityService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/identity/UserIdentityService.java
@@ -126,7 +126,7 @@ public class UserIdentityService implements CreateUserUseCase  {
         log.info("User Identity after password has been created: {}", userIdentity);
 //        blackListedTokenAdapter.blackListToken(createBlackList(token));
         log.info("done getting user identity frm token {}",userIdentity);
-        userIdentity = identityManagerOutPutPort.createPassword(userIdentity.getEmail(), password);
+        userIdentity = identityManagerOutPutPort.createPassword(UserIdentity.builder().email(userIdentity.getEmail()).password(password).build());
 //        blackListedTokenAdapter.blackListToken(createBlackList(token));
         return userIdentity;
     }
