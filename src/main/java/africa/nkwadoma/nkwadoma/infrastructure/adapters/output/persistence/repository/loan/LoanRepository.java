ba53738a7ace615158e9b2630618180828983153
Tade Emmanuel <tadeemmanuel08@gmail.com>
2024-12-31 09:39:16 +0100

    query formatting

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/loan/LoanRepository.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/loan/LoanRepository.java
@@ -7,15 +7,25 @@ import org.springframework.data.repository.query.*;
 
 public interface LoanRepository extends JpaRepository<LoanEntity, String> {
     @Query("""
-          select lr.id as id, l.userIdentity.firstName as firstName, l.userIdentity.lastName as lastName,
-          le.startDate as loanStartDate, o.name as referredBy, lr.loanAmountRequested as loanAmountRequested,
-          lr.createdDate as createdDate, l.loaneeLoanDetail.initialDeposit as initialDeposit, c.startDate as cohortStartDate, p.name as programName
+          select
+          lr.id as id,
+          l.userIdentity.firstName as firstName,
+          l.userIdentity.lastName as lastName,
+          le.startDate as loanStartDate,
+          o.name as referredBy,
+          lr.loanAmountRequested as loanAmountRequested,
+          lr.createdDate as createdDate,
+          l.loaneeLoanDetail.initialDeposit as initialDeposit,
+          c.startDate as cohortStartDate,
+          p.name as programName
+    
           from LoanEntity le
           join LoaneeEntity l on le.loaneeEntity.id = l.id
           join LoanRequestEntity lr on lr.loaneeEntity.id = l.id
           join CohortEntity c on l.cohortId = c.id
           join ProgramEntity p on c.programId = p.id
           join OrganizationEntity o on p.organizationIdentity.id = o.id
+    
           where o.id = :organizationId
     """)
     Page<LoanProjection> findAllByOrganizationId(@Param("organizationId") String organizationId, Pageable pageable);
