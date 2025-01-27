b02d6d0806a1f86d62ae8caf5aa34afe1cda00db
Tade Emmanuel <tadeemmanuel08@gmail.com>
2025-01-27 19:04:51 +0100

    validations for request body

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/investmentVehicle/CreateInvestmentVehicleRequest.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/investmentVehicle/CreateInvestmentVehicleRequest.java
@@ -1,16 +1,18 @@
 package africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.data.request.investmentVehicle;
 
+import africa.nkwadoma.nkwadoma.domain.enums.constants.*;
 import africa.nkwadoma.nkwadoma.domain.enums.investmentVehicle.InvestmentVehicleType;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
+
 @Setter
 @Getter
 public class CreateInvestmentVehicleRequest {
-
+    @Pattern(regexp = MeedlPatterns.CHARACTER_REGEX, message = "Name must not start or end with apostrophe or hyphen")
     private String name;
     private InvestmentVehicleType investmentVehicleType;
     @Size( max = 2500, message = "Investment vehicle mandate must not exceed 2500 characters")
@@ -21,7 +23,9 @@ public class CreateInvestmentVehicleRequest {
     private String trustee;
     private String custodian;
     private String bankPartner;
+    @Pattern(regexp = MeedlPatterns.CHARACTER_REGEX, message = "Fund manager must not start or end with apostrophe or hyphen")
     private String fundManager;
+    @Pattern(regexp = MeedlPatterns.CHARACTER_REGEX, message = "Sponsor must not start or end with apostrophe or hyphen")
     private String sponsors;
     private BigDecimal minimumInvestmentAmount;
 }
