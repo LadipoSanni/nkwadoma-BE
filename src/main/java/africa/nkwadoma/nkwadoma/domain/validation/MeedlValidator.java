b7a8084386fde02e08827242dea2d9f83e6fa283
Adeshina-Qudus <qudusa55@gmail.com>
2025-01-27 10:21:42 +0100

    error message fix

--- src/main/java/africa/nkwadoma/nkwadoma/domain/validation/MeedlValidator.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/validation/MeedlValidator.java
@@ -155,7 +155,7 @@ public class MeedlValidator {
         boolean isValid = pattern.matcher(name).matches();
         if (!isValid){
             log.error("Invalid name pattern: {}", name);
-            throw new MeedlException("Invalid name: {}. Name should not contain only numbers or special characters. Letters, numbers, ' - are allowed " + name);
+            throw new MeedlException("Invalid name: "+ name +". Name should not contain only numbers or special characters. Letters, numbers, _' - are allowed " );
         }
     }
     public static void validateEmailDomain(String inviteeEmail, String inviterEmail) throws MeedlException {
