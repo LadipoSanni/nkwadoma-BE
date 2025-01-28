da9993a098ce1b2c6e2d43599be0cd490c3292a7
Adeshina-Qudus <qudusa55@gmail.com>
2025-01-28 08:33:53 +0100

    fixed the request problem

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/education/CreateCohortRequest.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/education/CreateCohortRequest.java
@@ -17,12 +17,13 @@ import java.util.List;
 @Getter
 public class CreateCohortRequest {
 
-    @NotBlank(message = "Program Id is required")
+    @NotBlank(message = "Program id is required")
     private String programId;
     @Size(max = 2500, message = "cohort description must no go beyond 2500")
     private String cohortDescription;
     private BigDecimal tuitionAmount = BigDecimal.ZERO;
     @NotBlank(message = "Cohort name is required")
+    @Size( max = 200, message = "Cohort name must not exceed 200 characters")
     private String name;
     @NotNull(message = "Start date is required")
     private LocalDate startDate;
--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/investmentVehicle/CreateInvestmentVehicleRequest.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/investmentVehicle/CreateInvestmentVehicleRequest.java
@@ -1,21 +1,25 @@
 package africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.data.request.investmentVehicle;
 
 import africa.nkwadoma.nkwadoma.domain.enums.investmentVehicle.InvestmentVehicleType;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 @Setter
 @Getter
 public class CreateInvestmentVehicleRequest {
 
+    @Size( max = 200, message = "Investment vehicle name must not exceed 200 characters")
     private String name;
     private InvestmentVehicleType investmentVehicleType;
     @Size( max = 2500, message = "Investment vehicle mandate must not exceed 2500 characters")
     private String mandate;
-    private int tenure;
+    @Max(value = 999, message = "Tenure cannot exceed three digits.")
+    private BigInteger tenure;
     private BigDecimal size;
     private Float rate;
     private String trustee;
