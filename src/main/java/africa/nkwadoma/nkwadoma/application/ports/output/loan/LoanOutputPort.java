c29d50c9a2332395cfe9c3cdd06beff4e0c35a7a
Adeshina-Qudus <Adeshina-Qudus>
2025-01-29 13:18:02 +0100

    changes

--- src/main/java/africa/nkwadoma/nkwadoma/application/ports/output/loan/LoanOutputPort.java
+++ src/main/java/africa/nkwadoma/nkwadoma/application/ports/output/loan/LoanOutputPort.java
@@ -16,4 +16,6 @@ public interface LoanOutputPort {
     Page<Loan> findAllByOrganizationId(String organizationId, int pageSize, int pageNumber) throws MeedlException;
 
     Page<Loan> searchLoan(String programId, String organizationId, String name, int pageSize, int pageNumber) throws MeedlException;
+
+    Page<Loan> findAllLoan(int pageSize, int pageNumber) throws MeedlException;
 }
--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/loan/LoanRepository.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/loan/LoanRepository.java
@@ -84,4 +84,25 @@ public interface LoanRepository extends JpaRepository<LoanEntity, String> {
     Page<LoanProjection> findAllLoanOfferByLoaneeNameInOrganizationAndProgram( @Param("programId") String programId,
                                                                                @Param("organizationId") String organizationId,
                                                                                @Param("name") String name, Pageable pageRequest);
+
+    @Query("""
+          select
+          le.id as id,
+          le.startDate as startDate,
+          l.userIdentity.firstName as firstName,
+          l.userIdentity.lastName as lastName,
+          l.loaneeLoanDetail.initialDeposit as initialDeposit,
+          lr.createdDate as createdDate, lr.loanAmountRequested as loanAmountRequested,
+          c.name as cohortName, c.startDate as cohortStartDate, loe.dateTimeOffered as offerDate,
+          p.name as programName
+    
+          from LoanEntity le
+          join LoaneeEntity l on le.loaneeEntity.id = l.id
+          join LoanRequestEntity lr on lr.loaneeEntity.id = l.id
+          join LoanOfferEntity loe on l.id = loe.loanee.id
+          join CohortEntity c on l.cohortId = c.id
+          join ProgramEntity p on c.programId = p.id
+          join OrganizationEntity o on p.organizationIdentity.id = o.id
+    """)
+    Page<LoanProjection> findAllLoan(Pageable pageRequest);
 }
