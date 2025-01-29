74602d649339fc978375d4cfa5411390ac80572d
Adeshina-Qudus <Adeshina-Qudus>
2025-01-29 09:43:29 +0100

    changes

--- src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/loanManagement/LoanRequestAdapterTest.java
+++ src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/loanManagement/LoanRequestAdapterTest.java
@@ -9,6 +9,7 @@ import africa.nkwadoma.nkwadoma.domain.enums.*;
 import africa.nkwadoma.nkwadoma.domain.enums.loanEnums.*;
 import africa.nkwadoma.nkwadoma.domain.exceptions.*;
 import africa.nkwadoma.nkwadoma.domain.model.education.*;
+import africa.nkwadoma.nkwadoma.domain.model.education.LoanDetail;
 import africa.nkwadoma.nkwadoma.domain.model.identity.*;
 import africa.nkwadoma.nkwadoma.domain.model.loan.*;
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.loanEntity.*;
