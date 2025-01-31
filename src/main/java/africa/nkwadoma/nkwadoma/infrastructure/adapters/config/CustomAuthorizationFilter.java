eb183323e436fcc0245ee7bdb3b84e7da72377c2
Adeshina-Qudus <qudusa55@gmail.com>
2025-01-31 16:22:21 +0100

    worked on deactivated users

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/CustomAuthorizationFilter.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/CustomAuthorizationFilter.java
@@ -1,5 +1,9 @@
 package africa.nkwadoma.nkwadoma.infrastructure.adapters.config;
 
+import africa.nkwadoma.nkwadoma.application.ports.output.identity.IdentityManagerOutputPort;
+import africa.nkwadoma.nkwadoma.application.ports.output.identity.UserIdentityOutputPort;
+import africa.nkwadoma.nkwadoma.domain.exceptions.MeedlException;
+import africa.nkwadoma.nkwadoma.domain.model.identity.UserIdentity;
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.identityManager.BlackListedTokenAdapter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
@@ -23,6 +27,7 @@ import java.util.Map;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     private final BlackListedTokenAdapter blackListedTokenAdapter;
+    private final IdentityManagerOutputPort identityManagerOutputPort;
     private  final ObjectMapper objectMapper;
 
 
@@ -32,6 +37,23 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
         log.info("Entering CustomAuthorizationFilter for request: {}", request.getRequestURI());
         if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof Jwt) {
             Jwt jwt = (Jwt) authentication.getPrincipal();
+            try {
+                log.info("Finding user performing this action in the filter chain");
+                UserIdentity userIdentity = identityManagerOutputPort.getUserById(jwt.getClaimAsString("sub"));
+                log.info("Found user identity: {} and user is enabled {}", userIdentity.getEmail(), userIdentity.isEnabled());
+                if (!userIdentity.isEnabled()){
+                    log.warn("User is disabled and attempting to perform an action. User Email is: {} and user id is: {}", userIdentity.getEmail(), userIdentity.getId());
+                    userIdentity.setAccessToken(jwt.getTokenValue());
+                    identityManagerOutputPort.logout(userIdentity);
+                    log.warn("Disabled user forcefully logged out");
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType("application/json");
+                    response.getWriter().write(objectMapper.writeValueAsString(Map.of("Error", "Action not permitted. Contact admin.")));
+                    return;
+                }
+            }catch (Exception e) {
+                log.error("Error occurred in the filter chain trying to find user is enabled user {}",e.getMessage());
+            }
             if (isLoggedOutOnSystem(jwt.getTokenValue())) {
                 log.info("JWT token is blacklisted: {}", jwt.getTokenValue());
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
