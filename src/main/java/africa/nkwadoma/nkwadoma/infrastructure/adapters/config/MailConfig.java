a6eee46a2bf16d272ba8630818fe7e7e5d70fec8
Tade Emmanuel <tadeemmanuel08@gmail.com>
2025-02-05 16:09:44 +0100

    updated credit registry response

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/MailConfig.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/MailConfig.java
@@ -37,7 +37,7 @@ public class MailConfig {
         props.put("mail.smtp.timeout", "5000");
         props.put("mail.smtp.connectiontimeout", "5000");
         props.put("mail.smtp.writetimeout", "5000");
-        props.put("mail.smtp.ssl.enable", "false");
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
