feb0025ae79496969f13b466822e2b4dd470d317
Adeshina-Qudus <Adeshina-Qudus>
2025-02-27 16:48:27 +0100

    create notification test class

---
+++ src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/notification/NotificationAdapterTest.java
@@ -0,0 +1,15 @@
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.notification;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotificationAdapterTest {
+
+
+    @Test
+    void createNotification() {
+
+    }
+}
