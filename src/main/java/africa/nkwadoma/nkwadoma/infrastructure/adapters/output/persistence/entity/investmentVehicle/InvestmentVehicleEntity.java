12a0bdf96bb5f3feb62dc8d7092f7810fe9e156f
Adeshina-Qudus <Adeshina-Qudus>
2025-03-07 15:45:14 +0100

    commit

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/entity/investmentVehicle/InvestmentVehicleEntity.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/entity/investmentVehicle/InvestmentVehicleEntity.java
@@ -51,7 +51,6 @@ public class InvestmentVehicleEntity {
     private BigDecimal minimumInvestmentAmount;
     private LocalDate startDate;
     @Enumerated(EnumType.STRING)
-//    @Column(nullable = false, columnDefinition = "DRAFT")
     private InvestmentVehicleStatus investmentVehicleStatus;
     private String investmentVehicleLink;
     @OneToOne
