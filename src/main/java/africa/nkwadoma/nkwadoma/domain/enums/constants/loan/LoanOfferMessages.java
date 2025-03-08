8d0f0b1a7ec18e189513ca7b5c07bd1871258534
Ned <ned@workmail.com>
2025-03-08 18:57:14 +0100

    Update LoanOfferMessages.java

--- src/main/java/africa/nkwadoma/nkwadoma/domain/enums/constants/loan/LoanOfferMessages.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/enums/constants/loan/LoanOfferMessages.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public enum LoanOfferMessages {
 
-    LOAN_OFFER_NOT_FOUND("Loan offer not found"),
-    INVALID_LOAN_OFFER_ID("Please provide a valid loan offer id"),
-    LOAN_OFFER_CANNOT_BE_EMPTY("Loan offer cannot be empty or null"),
-    LOAN_OFFER_IS_NOT_ASSIGNED_TO_LOANEE("Loan offer is not assigned to loanee");
+    LOAN_OFFER_NOT_FOUND("Loan offer not found."),
+    INVALID_LOAN_OFFER_ID("Please provide a valid loan offer id."),
+    LOAN_OFFER_CANNOT_BE_EMPTY("Loan offer cannot be empty."),
+    LOAN_OFFER_IS_NOT_ASSIGNED_TO_LOANEE("Loan offer is not assigned to loanee.");
     private final String message;
 
     LoanOfferMessages(String message) {
