f4ad3de84cb6485e1912b79e7f39b8e6a67e7b68
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 <ned@workmail.com>
2025-03-17 13:38:07 +0100

    creating bank detail adapter in progress pulled

---
+++ src/main/java/africa/nkwadoma/nkwadoma/application/ports/output/bankDetail/BankDetailOutputPort.java
@@ -0,0 +1,4 @@
+package africa.nkwadoma.nkwadoma.application.ports.output.bankDetail;
+
+public interface BankDetailOutputPort {
+}
--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/bankDetail/BankDetailAdapter.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/bankDetail/BankDetailAdapter.java
@@ -1,4 +1,9 @@
 package africa.nkwadoma.nkwadoma.infrastructure.adapters.output.bankDetail;
 
-public class BankDetailAdapter {
+import africa.nkwadoma.nkwadoma.application.ports.output.bankDetail.BankDetailOutputPort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BankDetailAdapter implements BankDetailOutputPort {
+
 }
