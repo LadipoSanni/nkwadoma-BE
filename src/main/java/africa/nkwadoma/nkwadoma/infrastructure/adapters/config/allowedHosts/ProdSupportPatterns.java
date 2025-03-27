1343ed51acc446ee9c614ceaeda233b7672ce684
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 <ned@workmail.com>
2025-03-27 18:01:40 +0100

    application prod support added

---
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/allowedHosts/ProdSupportPatterns.java
@@ -0,0 +1,21 @@
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.config.allowedHosts;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("prodsupport")
+@Component
+public class ProdSupportPatterns implements AllowedHost{
+    @Value("${prodsupport}")
+    private String allowedHost;
+    @Value("${prodsupport2}")
+    private String allowedHost2;
+    @Override
+    public String[] getPatterns() {
+        return new String[]{
+                allowedHost,
+                allowedHost2
+        };
+    }
+}
