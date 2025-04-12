141367b88ce2fbfc328bf64227e4657bc740e62f
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-04-12 18:23:59 +0100

    view my investment detail

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/investmentVehicle/FinancierService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/investmentVehicle/FinancierService.java
@@ -47,7 +47,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @AllArgsConstructor
