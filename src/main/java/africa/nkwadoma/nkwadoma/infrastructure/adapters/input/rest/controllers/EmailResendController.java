ee71975c38cecb3373459a96332e7e54268aacb1
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 <ned@workmail.com>
2025-05-14 07:03:30 +0100

    made the action a post construct action

---
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/EmailResendController.java
@@ -0,0 +1,35 @@
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.controllers;
+
+import africa.nkwadoma.nkwadoma.application.ports.input.email.EmailResendUseCase;
+import africa.nkwadoma.nkwadoma.domain.exceptions.MeedlException;
+import africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.data.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import static africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.message.UrlConstant.BASE_URL;
+
+@Slf4j
+@RestController
+@RequestMapping(BASE_URL)
+@RequiredArgsConstructor
+public class EmailResendController {
+    @Autowired
+    private EmailResendUseCase emailResendUseCase;
+
+    @PostMapping("loanee/refer/email/resend/{email}")
+    @PreAuthorize("hasAuthority('ORGANIZATION_ADMIN')")
+    public ResponseEntity<ApiResponse<?>> resendEmail(@PathVariable @Valid String email) throws MeedlException {
+        emailResendUseCase.resendReferralEmail(email);
+        return ResponseEntity.ok(ApiResponse.<String>builder()
+                .message("Email sent successfully.").
+                statusCode(HttpStatus.OK.name()).build()
+        );
+    }
+
+}
