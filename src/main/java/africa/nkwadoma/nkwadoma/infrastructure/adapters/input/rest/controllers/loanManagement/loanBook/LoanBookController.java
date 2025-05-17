3dd27d9b99226ebc10b31072406b468dbb52eee4
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 <ned@workmail.com>
2025-05-17 22:06:36 +0100

    about to work on the invite loanee to work without invited by . or to reimplement for this usecase specifically

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/loanManagement/loanBook/LoanBookController.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/loanManagement/loanBook/LoanBookController.java
@@ -50,11 +50,12 @@ public class LoanBookController {
                                                              @PathVariable String cohortId,
                                                              @PathVariable String loanProductId
                                                             ) throws MeedlException {
-        log.info("Upload loan book. Api .... ");
+        log.info("Upload loan book. Api called .... ");
 //        LoanBook loanBook = loanBookRestMapper.map(cohortId, convertToTempFile(file), meedlUser.getClaimAsString("sub") );
 //        LoanBook loanBook = loanBookRestMapper.map(convertToTempFile(file));
         LoanBook loanBook = new LoanBook();
         loanBook.setFile(convertToTempFile(file));
+        loanBook.setLoanProductId(loanProductId);
         loanBook.setCohort(Cohort.builder().id(cohortId).createdBy(meedlUser.getClaimAsString("sub")).build());
         LoanBook loanBookReturned = loanBookUseCase.upLoadFile(loanBook);
         LoanBookResponse loanBookResponse = new LoanBookResponse();
