da799c159b416de7868c022cb577faf89f7f4ff1
Adeshina-Qudus <Adeshina-Qudus>
2025-05-22 00:38:00 +0100

    controller done

---
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/data/request/loanManagement/LoaneeDeferRequest.java
@@ -0,0 +1,11 @@
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.input.rest.data.request.loanManagement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoaneeDeferRequest {
+
+    private String loaneeId;
+}
