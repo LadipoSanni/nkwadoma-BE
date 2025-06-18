5223911e242aa33f5fef681dfc8cc68e1de45677
Adeshina-Qudus <qudusa55@gmail.com>
2025-06-18 12:57:05 +0100

    another fixex

--- src/main/resources/db/migration/V4__Investment_Vehicle_designation.sql
+++ src/main/resources/db/migration/V4__Investment_Vehicle_designation.sql
@@ -1,4 +1,13 @@
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT FROM pg_constraint
+        WHERE conname = 'fkixcyfka2uaf4ahgox5aq4898b'
+        AND conrelid = 'investment_vehicle_financier_entity_investment_vehicle_designat'::regclass
+    ) THEN
 ALTER TABLE investment_vehicle_financier_entity_investment_vehicle_designat
-DROP CONSTRAINT IF EXISTS fkixcyfka2uaf4ahgox5aq4898b;
+DROP CONSTRAINT fkixcyfka2uaf4ahgox5aq4898b;
+END IF;
+END $$;
 
 DROP TABLE IF EXISTS investment_vehicle_financier_entity_investment_vehicle_designat;
