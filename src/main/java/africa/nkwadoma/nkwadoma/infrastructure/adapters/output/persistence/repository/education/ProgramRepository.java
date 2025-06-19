6bdd497a8e36cd0f624a12d8569b44189b9027f5
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 <ned@workmail.com>
2025-06-19 16:44:47 +0100

    did uqniue names for programs in cohort

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/education/ProgramRepository.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/repository/education/ProgramRepository.java
@@ -3,6 +3,7 @@ package africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.repo
 import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.persistence.entity.education.ProgramEntity;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 import java.util.*;
 
@@ -11,6 +12,12 @@ public interface ProgramRepository extends JpaRepository<ProgramEntity, String> 
     List<ProgramEntity> findByNameContainingIgnoreCaseAndOrganizationIdentityId(String programName, String organizationId);
     List<ProgramEntity> findProgramEntitiesByOrganizationIdentityId(String organizationIdentityId);
     boolean existsByNameAndOrganizationIdentity_Id(String programName, String organizationId);
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END " +
+            "FROM ProgramEntity p " +
+            "WHERE LOWER(p.name) = LOWER(:programName) " +
+            "AND p.organizationIdentity.id = :organizationId")
+    boolean existsByNameIgnoreCaseAndOrganizationIdentityId(@Param("programName") String programName,
+                                                            @Param("organizationId") String organizationId);
 
     Page<ProgramEntity> findAllByOrganizationIdentityId(String organizationId, Pageable pageable);
 }
