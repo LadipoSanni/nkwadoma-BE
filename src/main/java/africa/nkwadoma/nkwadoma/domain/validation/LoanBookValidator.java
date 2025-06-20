4fb171715962290ab171452ae04b4e2a3a75c0de
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 <ned@workmail.com>
2025-06-20 21:37:07 +0100

    worked on basic phone number validation

--- src/main/java/africa/nkwadoma/nkwadoma/domain/validation/LoanBookValidator.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/validation/LoanBookValidator.java
@@ -46,10 +46,15 @@ public class LoanBookValidator {
         for (Loanee loanee : convertedLoanees) {
             validateFileBvn(loanee.getUserIdentity());
             validateFileNin(loanee.getUserIdentity());
+            validatePhoneNumber(loanee.getUserIdentity());
             validateLoanProductExist(loanee);
             validateAmount(loanee);
         }
 
+    }
+
+    private void validatePhoneNumber(UserIdentity userIdentity) throws MeedlException {
+        MeedlValidator.validateElevenDigits(userIdentity.getPhoneNumber(),"User with email "+userIdentity.getEmail()+ " has invalid phone number.");
     }
 
     private void validateAmount(Loanee loanee) throws MeedlException {
@@ -86,13 +91,13 @@ public class LoanBookValidator {
 
     }
     private String validateFileAndEncryptBvnOrNin(String bvnOrNin, String errorMessage) throws MeedlException {
-        MeedlValidator.validateBvnOrNin(bvnOrNin, errorMessage);
+        MeedlValidator.validateElevenDigits(bvnOrNin, errorMessage);
 
         return encryptValue(bvnOrNin, errorMessage);
     }
     private String encryptValue(String value, String errorMessage) {
         try {
-            MeedlValidator.validateBvnOrNin(value, errorMessage);
+            MeedlValidator.validateElevenDigits(value, errorMessage);
             return aesOutputPort.encryptAES(value.trim());
         } catch (MeedlException e) {
             log.error("Unable to encrypt value {}", value);
