332904c99bb34c76ef514dd972c8fa6031a7de35
Adeshina-Qudus <qudusa55@gmail.com>
2025-06-20 13:51:23 +0100

    constraint removed and added new one

--- src/main/resources/db/migration/V4.4__Meedl_user_constraint.sql
+++ src/main/resources/db/migration/V4.4__Meedl_user_constraint.sql
@@ -1,3 +1,15 @@
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'meedl_user_role_check'
+        AND conrelid = 'meedl_user'::regclass
+    ) THEN
 ALTER TABLE meedl_user DROP CONSTRAINT meedl_user_role_check;
+END IF;
+END $$;
+
+-- Add a new constraint with updated roles
 ALTER TABLE meedl_user ADD CONSTRAINT meedl_user_role_check
     CHECK (role IN ('PORTFOLIO_MANAGER', 'ORGANIZATION_ADMIN', 'LOANEE', 'FINANCIER'));
