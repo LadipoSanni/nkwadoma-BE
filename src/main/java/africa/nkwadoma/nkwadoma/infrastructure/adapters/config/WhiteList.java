022f8d9c46bea4ffd179838ab901c8cf0d4471e5
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 <ned@workmail.com>
2025-06-24 11:16:20 +0100

    worked on the qhite list url change

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/WhiteList.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/config/WhiteList.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 
 public class WhiteList {
-//    @Value("${api.version}")
-//    private static String api_version;
+
     public static final String[] patterns = {
             "/swagger-ui/index.html",
             "/swagger-ui.html",
@@ -15,13 +14,13 @@ public class WhiteList {
             "/v3/api-docs",
             "/webjars/**",
             "/actuator/health",
-//            api_version+ "auth/login",
-//            api_version+ "auth/password/create",
-//            api_version+ "auth/password/forgotPassword",
-            "/api/v1/auth/password/forgotPassword",
-//            api_version+ "auth/refresh-token",
-//            api_version+ "auth/password/reset",
-//            api_version+ "identity/verification/failure-record/create",
-//            api_version+ "investment-vehicle/detail/link/**"
+            "/auth/login",
+            "/auth/password/create",
+            "/auth/password/forgotPassword",
+            "/auth/password/forgotPassword",
+            "/auth/refresh-token",
+            "/auth/password/reset",
+            "/identity/verification/failure-record/create",
+            "/investment-vehicle/detail/link/**"
     };
 }
