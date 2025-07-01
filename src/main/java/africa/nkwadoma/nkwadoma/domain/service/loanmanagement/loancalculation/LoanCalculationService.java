bcd5bf3458ae44b2922979750f073501333c7943
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 <ned@workmail.com>
2025-07-01 16:10:06 +0100

    created new branch about to pull

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loancalculation/LoanCalculationService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loancalculation/LoanCalculationService.java
@@ -3,12 +3,14 @@ package africa.nkwadoma.nkwadoma.domain.service.loanmanagement.loancalculation;
 import africa.nkwadoma.nkwadoma.application.ports.input.loanmanagement.loancalculation.LoanCalculationUseCase;
 import africa.nkwadoma.nkwadoma.domain.exceptions.MeedlException;
 import africa.nkwadoma.nkwadoma.domain.model.loan.loanBook.LoanPeriodRecord;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
+@Slf4j
 @Component
 public class LoanCalculationService implements LoanCalculationUseCase {
     @Override
@@ -60,6 +62,35 @@ public class LoanCalculationService implements LoanCalculationUseCase {
         validateInterestRate(interestRate, "Interest rate");
         return interestRate / 12;
     }
+    @Override
+    public BigDecimal calculateManagementOrProcessingFee(BigDecimal loanAmountRequested, double mgtFeeInPercentage) throws MeedlException {
+        validateAmount(loanAmountRequested, "Loan Amount Requested");
+        validateInterestRate(mgtFeeInPercentage, "Management Fee Percentage");
+
+        BigDecimal percentageDecimal = BigDecimal.valueOf(mgtFeeInPercentage).divide(BigDecimal.valueOf(100), 10, RoundingMode.HALF_UP);
+        return loanAmountRequested.multiply(percentageDecimal).setScale(8, RoundingMode.HALF_UP);
+    }
+    public BigDecimal calculateCreditLife(BigDecimal loanAmountRequested, int creditLifePercentage, int loanTenureMonths) throws MeedlException {
+        validateAmount(loanAmountRequested, "Loan Amount Requested");
+        validateInterestRate(creditLifePercentage, "Credit Life Percentage");
+        validateLoanTenure(loanTenureMonths);
+
+        BigDecimal percentage = BigDecimal.valueOf(creditLifePercentage).divide(BigDecimal.valueOf(100), 10, RoundingMode.HALF_UP);
+
+        double tenureYears = loanTenureMonths / 12.0;
+        int multiplier = Math.max(1, (int) Math.ceil(tenureYears));
+
+        return loanAmountRequested
+                .multiply(percentage)
+                .multiply(BigDecimal.valueOf(multiplier))
+                .setScale(8, RoundingMode.HALF_UP);
+    }
+
+    private void validateLoanTenure(int tenure) throws MeedlException {
+        if (tenure < 0) {
+            throw new MeedlException("Loan Tenure must not be negative.");
+        }
+    }
 
     private void validateLoanPeriodRecord(LoanPeriodRecord record) throws MeedlException {
         if (record == null){
@@ -78,20 +109,36 @@ public class LoanCalculationService implements LoanCalculationUseCase {
 
     private void validateInterestRate(int interestRate, String name) throws MeedlException {
         if (interestRate < 0) {
+            log.error("{} must not be negative. In validate interest rate.",name);
             throw new MeedlException(name+" must not be negative.");
         }
         if (interestRate > 100) {
+            log.error("{} must not exceed 100. In validate interest rate.", name );
             throw new MeedlException(name+" must not exceed 100.");
         }
     }
 
+    private void validateInterestRate(double interestRate, String name) throws MeedlException {
+        if (interestRate < 0) {
+            log.error("{} must not be negative. In validate interest rate as double",name);
+            throw new MeedlException(name+" must not be negative.");
+        }
+        if (interestRate > 100) {
+            log.error("{} must not exceed 100. In validate interest rate. as double", name );
+            throw new MeedlException(name+" must not exceed 100.");
+        }
+    }
+
+
+
     private void validateAmount(BigDecimal amount, String name) throws MeedlException {
         if (amount == null) {
+            log.error( "{} must not be null. In validate amount", name);
             throw new MeedlException(name + " must not be null.");
         }
         if (amount.compareTo(BigDecimal.ZERO) < 0) {
+            log.error( "{} must not be negative. In validate amount", name);
             throw new MeedlException(name + " must not be negative.");
         }
-
     }
 }
