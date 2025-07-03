83a6b44627247e2e31700c9ee7db0d7bff146a84
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 <ned@workmail.com>
2025-07-03 10:07:36 +0100

    removed imports that are no longer needed

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loancalculation/LoanCalculationService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loancalculation/LoanCalculationService.java
@@ -1,16 +1,8 @@
 package africa.nkwadoma.nkwadoma.domain.service.loanmanagement.loancalculation;
 
 import africa.nkwadoma.nkwadoma.application.ports.input.loanmanagement.loancalculation.LoanCalculationUseCase;
-import africa.nkwadoma.nkwadoma.domain.exceptions.MeedlException;
-import africa.nkwadoma.nkwadoma.domain.model.loan.loanBook.LoanPeriodRecord;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.math3.stat.StatUtils;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Component
