3ab690a8817c674ecff3ff40845cca51b5088c94
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 <ned@workmail.com>
2025-07-04 15:12:43 +0100

    worked on calculate total repayment for a list of repayments

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loancalculation/LoanCalculationService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loancalculation/LoanCalculationService.java
@@ -6,6 +6,7 @@ import africa.nkwadoma.nkwadoma.domain.model.loan.loanBook.RepaymentHistory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -24,13 +25,13 @@ public class LoanCalculationService implements LoanCalculationUseCase {
         log.info("Repayments are not empty and the sorting has started. The number of the repayment is :{}", repayments.size());
         for (RepaymentHistory repayment : repayments) {
             if (repayment == null) {
-                log.warn("Repayment history cannot be null");
-                throw new MeedlException("Repayment history cannot be null");
+                log.warn("Repayment history cannot be null, before sorting repayment by date.");
+                throw new MeedlException("Repayment history must be provided and not empty.");
             }
             log.info("Date of this repayment is : {}", repayment.getPaymentDateTime());
             if (repayment.getPaymentDateTime() == null) {
-                log.warn("Payment date cannot be null");
-                throw new MeedlException("Payment date cannot be null");
+                log.warn("Payment date cannot be null, before sorting repayment by date.");
+                throw new MeedlException("Payment date must be provided");
             }
         }
 
@@ -38,7 +39,35 @@ public class LoanCalculationService implements LoanCalculationUseCase {
 
         return repayments;
     }
+    public BigDecimal calculateTotalAmountRepaid(List<RepaymentHistory> repayments) throws MeedlException {
+        if (repayments == null || repayments.isEmpty()) {
+            log.warn("Repayments was null when calculating total amount repaid");
+            return BigDecimal.ZERO;
+        }
+        BigDecimal totalRepaymentAmount = BigDecimal.ZERO;
 
+        for (RepaymentHistory repayment : repayments) {
+            validationForCalculatingTotalAmountRepaid(repayment);
+             totalRepaymentAmount = totalRepaymentAmount.add(repayment.getAmountPaid());
+        }
+        repayments.get(0).setTotalAmountRepaid(totalRepaymentAmount);
+        return totalRepaymentAmount;
+    }
 
+    private static void validationForCalculatingTotalAmountRepaid(RepaymentHistory repayment) throws MeedlException {
+        if (repayment == null) {
+            log.warn("Repayment history cannot be null, while calculating total amount repaid");
+            throw new MeedlException("Repayment history must be provided.");
+        }
+        log.info("Amount repaid {} on {}", repayment.getAmountPaid(), repayment.getPaymentDateTime());
+        if (repayment.getAmountPaid() == null) {
+            log.warn("Payment amount cannot be null");
+            throw new MeedlException("Payment amount must be provided.");
+        }
+        if (repayment.getAmountPaid().compareTo(BigDecimal.ZERO) < 0) {
+            log.warn("Amount paid on {} can not be negative: {}", repayment.getPaymentDateTime(), repayment.getAmountPaid());
+            throw new MeedlException("Amount paid can not be negative : "+ repayment.getAmountPaid());
+        }
+    }
 
 }
