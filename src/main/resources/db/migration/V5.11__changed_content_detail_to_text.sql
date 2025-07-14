ad0f5e451eefe2dcecc1a5241a5f05a8f15b09fa
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-07-14 17:15:44 +0100

    removed test that are no loanger relevant

--- src/main/resources/db/migration/V5.11__changed_content_detail_to_text.sql
+++ src/main/resources/db/migration/V5.11__changed_content_detail_to_text.sql
@@ -1,5 +1,14 @@
 ALTER TABLE meedl_notification_entity
     ALTER COLUMN content_detail TYPE TEXT USING content_detail::TEXT;
 
-ALTER TABLE meedl_notification_entity
-DROP CONSTRAINT meedl_notification_entity_notification_flag_check;
+ DO $$
+BEGIN
+    IF EXISTS (
+       SELECT 1
+       FROM pg_constraint
+       WHERE conname = 'meedl_notification_entity_notification_flag_check'
+    ) THEN
+        ALTER TABLE meedl_notification_entity
+        DROP CONSTRAINT meedl_notification_entity_notification_flag_check;
+    END IF;
+END$$;
