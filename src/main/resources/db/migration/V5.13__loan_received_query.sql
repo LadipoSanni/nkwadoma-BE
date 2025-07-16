876439b40cda8bbf0116f48980a4f617c8e6bd6c
Adeshina-Qudus <qudusa55@gmail.com>
2025-07-16 22:13:42 +0100

    fixed

--- src/main/resources/db/migration/V5.13__loan_received_query.sql
+++ src/main/resources/db/migration/V5.13__loan_received_query.sql
@@ -4,7 +4,7 @@ SET
         SELECT MAX(lo.amount_approved)
         FROM loan_offer_entity lo
                  JOIN loan_entity l ON l.loan_offer_id = lo.id
-                 JOIN cohort_loanee_entity cl ON cl.cl.loanee_loan_detail_id = lld.id
+                 JOIN cohort_loanee_entity cl ON cl.loanee_loan_detail_id = lld.id
         WHERE lld.id = cl.loanee_loan_detail_id
     ),
     amount_outstanding = GREATEST(
@@ -12,4 +12,4 @@ SET
                 SELECT lld.amount_received - lld.amount_repaid
             ),
             0
-                         );
+     );
