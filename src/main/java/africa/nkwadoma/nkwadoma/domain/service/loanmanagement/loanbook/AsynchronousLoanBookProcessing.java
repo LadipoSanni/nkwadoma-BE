78959fbad335e97e57887bdb964e5b02568b4fe7
Adeshina-Qudus <qudusa55@gmail.com>
2025-07-18 11:15:58 +0100

    still in training count

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loanbook/AsynchronousLoanBookProcessing.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/loanmanagement/loanbook/AsynchronousLoanBookProcessing.java
@@ -144,6 +144,7 @@ public class AsynchronousLoanBookProcessing implements AsynchronousLoanBookProce
         log.info("Number of loanees in a cohort on upload {}", savedCohort.getNumberOfLoanees() + loanees.size());
         savedCohort.setNumberOfLoanees(savedCohort.getNumberOfLoanees() + loanees.size());
         savedCohort.setNumberOfLoanRequest(savedCohort.getNumberOfLoanRequest() + loanees.size());
+        savedCohort.setStillInTraining(savedCohort.getStillInTraining() + loanees.size());
         savedCohort.setNumberOfReferredLoanee(savedCohort.getNumberOfReferredLoanee() + loanees.size());
         log.info("Number of loanees in the cohort updated before save {}", savedCohort.getNumberOfLoanees());
         cohortOutputPort.save(savedCohort);
