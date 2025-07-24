4b5277b66231d8f70d4f83ac278f6faa56eab3c2
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-07-24 17:47:05 +0100

    view fund stack holder controller

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/InvestmentVehicleController.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/input/rest/controllers/InvestmentVehicleController.java
@@ -263,6 +263,17 @@ public class InvestmentVehicleController {
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
+    @GetMapping("fund-stack-holders")
+    public ResponseEntity<ApiResponse<?>> viewFundShareHolders() throws MeedlException {
 
+        FundStakeHolder fundStakeHolder =
+                investmentVehicleUseCase.viewFundStakeHolders();
+        ApiResponse<FundStakeHolder> apiResponse = ApiResponse.<FundStakeHolder>builder()
+                .data(fundStakeHolder)
+                .message("Fund stake holder returned successfully")
+                .statusCode(HttpStatus.OK.toString())
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
 
 }
