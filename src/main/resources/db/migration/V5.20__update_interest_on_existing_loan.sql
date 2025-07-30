e2b945255f926e2d6c71842e5e89ca5b407dc564
Adeshina-Qudus <qudusa55@gmail.com>
2025-07-30 12:47:15 +0100

    done with migration

--- src/main/resources/db/migration/V5.20__update_interest_on_existing_loan.sql
+++ src/main/resources/db/migration/V5.20__update_interest_on_existing_loan.sql
@@ -1,61 +1,74 @@
+-- Update loans
 UPDATE loanee_loan_detail_entity llde
 SET
---     interest_incurred = (
---         CASE
---             WHEN loan_start_date IS NOT NULL and updated_at IS NULL
---                 AND interest_incurred = 0 AND amount_outstanding > 0
---                 THEN COALESCE(interest_incurred, 0) + (
---                 amount_outstanding * (interest_rate / 100.0 / 365.0) *
---                 (DATE_PART('day', AGE(CURRENT_DATE, loan_start_date)) - 1)
---                 )
---
---             WHEN loan_start_date IS NOT NULL and  updated_at IS NOT NULL
---                 AND amount_outstanding > 0 AND interest_incurred = 0 AND amount_repaid = 0
---                 THEN COALESCE(interest_incurred, 0) + (
---                 amount_outstanding * (interest_rate / 100.0 / 365.0) *
---                 (DATE_PART('day', AGE(CURRENT_DATE, loan_start_date)) - 1)
---                 )
---
---             WHEN updated_at IS NULL AND loan_start_date IS NOT NULL
---                 AND interest_incurred > 0 AND amount_outstanding > 0 AND amount_repaid > 0
---                 THEN (
---                 COALESCE(amount_received, 0) * (interest_rate / 100.0 / 365.0) *
---                 (DATE_PART('day', AGE(CURRENT_DATE, loan_start_date)) - 1)
---                 )
---
---             WHEN loan_start_date IS NOT NULL and  updated_at IS NOT NULL
---                  AND amount_outstanding > 0 AND interest_incurred > 0
---                  THEN COALESCE(interest_incurred, 0) + (
---                  amount_outstanding * (interest_rate / 100.0 / 365.0) *
---                  (DATE_PART('day', AGE(CURRENT_DATE, updated_at::date)) - 1)
---                  )
---
---             ELSE COALESCE(interest_incurred, 0)
---             END)
---         ),
-    amount_outstanding =
+    interest_incurred = (
         CASE
-             WHEN loan_start_date IS NOT NULL and updated_at IS NULL
-             AND interest_incurred = 0 AND amount_outstanding > 0
-             THEN COALESCE(amount_outstanding, 0) + COALESCE(interest_incurred, 0)
+            WHEN loan_start_date IS NOT NULL AND updated_at IS NULL
+                AND interest_incurred = 0 AND amount_outstanding > 0
+                THEN COALESCE(interest_incurred, 0) + (
+                amount_outstanding * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - loan_start_date::date)
+                )
 
-             WHEN loan_start_date IS NOT NULL and  updated_at IS NOT NULL
-             AND amount_outstanding > 0 AND interest_incurred = 0 AND amount_repaid = 0
-             THEN COALESCE(amount_outstanding, 0) + COALESCE(interest_incurred, 0)
+            WHEN loan_start_date IS NOT NULL AND updated_at IS NOT NULL
+                AND amount_outstanding > 0 AND interest_incurred = 0 AND amount_repaid = 0
+                THEN COALESCE(interest_incurred, 0) + (
+                amount_outstanding * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - loan_start_date::date)
+                )
 
-             WHEN updated_at IS NULL AND loan_start_date IS NOT NULL
-             AND interest_incurred > 0 AND amount_outstanding > 0 AND amount_repaid > 0
-             THEN COALESCE(amount_received, 0) - COALESCE(amount_repaid, 0) + COALESCE(interest_incurred, 0)
+            WHEN updated_at IS NULL AND loan_start_date IS NOT NULL
+                AND interest_incurred > 0 AND amount_outstanding > 0 AND amount_repaid > 0
+                THEN
+                amount_received * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - loan_start_date::date)
 
-             WHEN loan_start_date IS NOT NULL and  updated_at IS NOT NULL
-             AND amount_outstanding > 0 AND interest_incurred > 0
-             THEN COALESCE(amount_outstanding, 0) + COALESCE(interest_incurred, 0)
+            WHEN loan_start_date IS NOT NULL AND updated_at IS NOT NULL
+                AND amount_outstanding > 0 AND interest_incurred > 0
+                THEN COALESCE(interest_incurred, 0) + (
+                amount_outstanding * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - updated_at::date)
+                )
 
+            ELSE COALESCE(interest_incurred, 0)
+            END
+        ),
+    amount_outstanding = (
+        CASE
+            WHEN loan_start_date IS NOT NULL AND updated_at IS NULL
+                AND interest_incurred = 0 AND amount_outstanding > 0
+                THEN COALESCE(amount_outstanding, 0) + (
+                amount_outstanding * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - loan_start_date::date)
+                )
 
-             ELSE amount_outstanding
-        END,
+            WHEN loan_start_date IS NOT NULL AND updated_at IS NOT NULL
+                AND amount_outstanding > 0 AND interest_incurred = 0 AND amount_repaid = 0
+                THEN COALESCE(amount_outstanding, 0) + (
+                amount_outstanding * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - loan_start_date::date)
+                )
+
+            WHEN updated_at IS NULL AND loan_start_date IS NOT NULL
+                AND interest_incurred > 0 AND amount_outstanding > 0 AND amount_repaid > 0
+                THEN COALESCE(amount_received, 0) - COALESCE(amount_repaid, 0) + (
+                amount_received * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - loan_start_date::date)
+                )
+
+            WHEN loan_start_date IS NOT NULL AND updated_at IS NOT NULL
+                AND amount_outstanding > 0 AND interest_incurred > 0
+                THEN COALESCE(amount_outstanding, 0) + (
+                amount_outstanding * (interest_rate / 100.0 / 365.0) *
+                ((CURRENT_DATE - INTERVAL '1 day')::date - updated_at::date)
+                )
+
+            ELSE COALESCE(amount_outstanding, 0)
+            END
+        ),
     updated_at = CURRENT_TIMESTAMP
 WHERE
     amount_outstanding IS NOT NULL
   AND amount_outstanding > 0
-  AND interest_rate IS NOT NULL;
+  AND interest_rate IS NOT NULL
+  AND (updated_at IS NULL OR updated_at::date != CURRENT_DATE);
