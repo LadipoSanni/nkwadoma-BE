ef7aec7c108db653bf0aa81c5ff4352a4a7be7ba
Adeshina-Qudus <qudusa55@gmail.com>
2025-07-31 10:59:36 +0100

    createrd mapper

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/loanmanagement/loanBook/DailyInterestAdapter.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/loanmanagement/loanBook/DailyInterestAdapter.java
@@ -3,6 +3,7 @@ package africa.nkwadoma.nkwadoma.infrastructure.adapters.output.loanmanagement.l
 import africa.nkwadoma.nkwadoma.application.ports.output.loanmanagement.loanbook.DailyInterestOutputPort;
 import africa.nkwadoma.nkwadoma.domain.exceptions.MeedlException;
 import africa.nkwadoma.nkwadoma.domain.model.loan.loanBook.DailyInterest;
+import africa.nkwadoma.nkwadoma.infrastructure.adapters.output.mapper.loan.loanBook.DailyInterestMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DailyInterestAdapter implements DailyInterestOutputPort {
 
+    private DailyInterestMapper dailyInterestMapper;
 
     @Override
     public DailyInterest save(DailyInterest dailyInterest) throws MeedlException {
         dailyInterest.validate();
+
+        DailyInterestEntity dailyInterestEntity =
+                dailyInterestMapper.toDailyInterestEntity(dailyInterest);
+
         return null;
     }
 }
