7979eb58827f26d43781956036c5bf3c1b2654e6
Adeshina-Qudus <qudusa55@gmail.com>
2025-08-05 17:16:51 +0100

    new identity role added

--- src/main/java/africa/nkwadoma/nkwadoma/domain/enums/IdentityRole.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/enums/IdentityRole.java
@@ -1,8 +1,13 @@
 package africa.nkwadoma.nkwadoma.domain.enums;
 
 public enum IdentityRole {
+    MEEDL_ASSOCIATE,
+    MEEDL_SUPER_ADMIN,
+    MEEDL_ADMIN,
     PORTFOLIO_MANAGER,
     ORGANIZATION_ADMIN,
+    ORGANIZATION_ASSOCIATE,
+    ORGANIZATION_SUPER_ADMIN,
     LOANEE,
-    FINANCIER
+    FINANCIER,
 }
