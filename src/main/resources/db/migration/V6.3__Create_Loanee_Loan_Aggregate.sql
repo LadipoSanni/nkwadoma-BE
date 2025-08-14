3c65b545a76618cb40911d2e5f0a5776f387ed80
Adeshina-Qudus <qudusa55@gmail.com>
2025-08-14 13:59:28 +0100

    undo mistake

--- src/main/resources/db/migration/V6.3__Create_Loanee_Loan_Aggregate.sql
+++ src/main/resources/db/migration/V6.3__Create_Loanee_Loan_Aggregate.sql
@@ -1,7 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS loanee_loan_aggregate_entity (
-     total_amount_outstanding DECIMAL(19,2) NOT NULL DEFAULT 0.00,
+    id VARCHAR(36) PRIMARY KEY,
+    total_amount_outstanding DECIMAL(19,2) NOT NULL DEFAULT 0.00,
     historical_debt DECIMAL(19,2) NOT NULL DEFAULT 0.00,
     number_of_loans INT NOT NULL DEFAULT 0,
     loanee_id VARCHAR(36),
