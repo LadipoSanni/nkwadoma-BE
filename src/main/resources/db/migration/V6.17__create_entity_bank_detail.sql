5c2c20de9e42c79df0a911957320242ca672f1ec
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 <ned@workmail.com>
2025-08-25 09:50:09 +0100

    worked on adding entity bank detail to script

---
+++ src/main/resources/db/migration/V6.17__create_entity_bank_detail.sql
@@ -0,0 +1,15 @@
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+-- Create table for EntityBankDetail
+CREATE TABLE entity_bank_detail (
+                                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                                    entity_id VARCHAR(255) NOT NULL,
+                                    bank_detail_id VARCHAR(255) UNIQUE, -- one-to-one relationship
+
+                                    CONSTRAINT fk_entity_bank_detail_bank_detail
+                                        FOREIGN KEY (bank_detail_id)
+                                            REFERENCES bank_detail_entity(id)
+                                            ON DELETE CASCADE
+);
