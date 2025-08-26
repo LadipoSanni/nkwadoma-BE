84a3a379c13421627d929e0f5bf4e596844e70f5
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 <ned@workmail.com>
2025-08-26 14:31:14 +0100

    fixed build for dev flyway script

--- src/main/resources/db/migration/V6.18__ALTER_ORGANIZATION_ENTITY.sql
+++ src/main/resources/db/migration/V6.18__ALTER_ORGANIZATION_ENTITY.sql
@@ -1,12 +1,15 @@
 ALTER TABLE organization
     ADD COLUMN requested_invitation_date TIMESTAMP;
 
+
 UPDATE organization
 SET requested_invitation_date =
         CASE
             WHEN UPPER(activation_status) = 'PENDING_APPROVAL'
                 THEN NOW()
-            ELSE invited_date
+            WHEN invited_date IS NOT NULL
+                THEN invited_date
+            ELSE NOW()
             END;
 
 ALTER TABLE organization
