aa1df33af035e76ac7d959dfa6cabd481b0e5562
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 <ned@workmail.com>
2025-08-27 09:01:42 +0100

    worked on the script for flyway

--- src/main/resources/db/migration/V6.19__alter_dob_beneficial_ower_added_financier_bank_detail.sql
+++ src/main/resources/db/migration/V6.19__alter_dob_beneficial_ower_added_financier_bank_detail.sql
@@ -4,7 +4,7 @@ ALTER COLUMN beneficial_owner_date_of_birth TYPE DATE
     USING beneficial_owner_date_of_birth::DATE;
 
 
-CREATE TABLE financier_bank_detail_entity (
+CREATE TABLE IF NOT EXISTS financier_bank_detail_entity (
         id VARCHAR(255) PRIMARY KEY NOT NULL,
         financier_entity_id VARCHAR(255) REFERENCES financier_entity(id),
         bank_detail_entity_id VARCHAR(255) REFERENCES bank_detail_entity(id)
@@ -12,14 +12,14 @@ CREATE TABLE financier_bank_detail_entity (
 
 CREATE TABLE organization_bank_detail_entity (
       id VARCHAR(255) PRIMARY KEY NOT NULL,
-      organization_entity_id VARCHAR(255) REFERENCES organization_entity(id),
+      organization_id VARCHAR(255) REFERENCES organization(id),
       bank_detail_entity_id VARCHAR(255) REFERENCES bank_detail_entity(id)
 );
 CREATE TABLE loanee_bank_detail_entity (
     id VARCHAR(255) PRIMARY KEY  NOT NULL,
     loanee_entity_id VARCHAR(255) REFERENCES loanee_entity(id),
     bank_detail_entity_id VARCHAR(255) REFERENCES bank_detail_entity(id)
-)
+);
 -- Change DOB column from TIMESTAMP to DATE
 ALTER TABLE beneficial_owner_entity
 ALTER COLUMN beneficial_owner_date_of_birth TYPE DATE
