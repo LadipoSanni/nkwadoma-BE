c27f25c5977fffe4b272036e8285757f78b91bb4
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 <ned@workmail.com>
2025-08-29 14:51:48 +0100

    worked on password change

--- src/main/java/africa/nkwadoma/nkwadoma/domain/service/identity/UserIdentityService.java
+++ src/main/java/africa/nkwadoma/nkwadoma/domain/service/identity/UserIdentityService.java
@@ -205,10 +205,6 @@ public class UserIdentityService implements UserUseCase {
     public void changePassword(UserIdentity userIdentity) throws MeedlException {
         MeedlValidator.validateObjectInstance(userIdentity, IdentityMessages.USER_IDENTITY_CANNOT_BE_NULL.getMessage());
         validatePasswordsForChangePassword(userIdentity);
-        if (userIdentity.getNewPassword().equals(userIdentity.getPassword())){
-            log.warn("{}", UserMessages.NEW_PASSWORD_AND_CURRENT_PASSWORD_CANNOT_BE_SAME.getMessage());
-            throw new IdentityException(UserMessages.NEW_PASSWORD_AND_CURRENT_PASSWORD_CANNOT_BE_SAME.getMessage());
-        }
         if(checkNewPasswordMatchLastFive(userIdentity)){
             throw new IdentityException(PASSWORD_NOT_ACCEPTED.getMessage());
         }
@@ -221,9 +217,9 @@ public class UserIdentityService implements UserUseCase {
     }
 
     private void validatePasswordsForChangePassword(UserIdentity userIdentity) throws MeedlException {
-        if (userIdentity.getPassword().equals(userIdentity.getNewPassword())){
-            log.error("Old an new password in change password is the same.");
-            throw new MeedlException("This password cannot be used");
+        if (userIdentity.getNewPassword().equals(userIdentity.getPassword())){
+            log.warn("{}", UserMessages.NEW_PASSWORD_AND_CURRENT_PASSWORD_CANNOT_BE_SAME.getMessage());
+            throw new IdentityException(UserMessages.NEW_PASSWORD_AND_CURRENT_PASSWORD_CANNOT_BE_SAME.getMessage());
         }
         String currentPassword = tokenUtils.decryptAES(userIdentity.getPassword(), "Invalid password entered for current password");
         try{
