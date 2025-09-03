2e4a117ff8e9eddfd8e8b4d90ae541fd022efb51
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 <ned@workmail.com>
2025-09-03 12:20:17 +0100

    removed space

--- src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/investmentvehicle/InvestmentVehicleAdapterTest.java
+++ src/test/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/investmentvehicle/InvestmentVehicleAdapterTest.java
@@ -206,7 +206,7 @@ class InvestmentVehicleAdapterTest {
             log.info("{} {}", exception.getClass().getName(), exception.getMessage());
         }
         assertNotNull(investmentVehicles);
-`        assertFalse(investmentVehicles.isEmpty());
+        assertFalse(investmentVehicles.isEmpty());
         assertThat(investmentVehicles).allMatch(investmentVehicle-> investmentVehicle.getInvestmentVehicleType().equals(InvestmentVehicleType.ENDOWMENT));
     }
     @Test
