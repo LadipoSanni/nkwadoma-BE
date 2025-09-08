bc74b63158d924b4791e7460f88d2233d34cdf82
Adeshina-Qudus <qudusa55@gmail.com>
2025-09-08 17:27:07 +0100

    entity chages

--- src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/entity/meedlportfolio/DemographyEntity.java
+++ src/main/java/africa/nkwadoma/nkwadoma/infrastructure/adapters/output/persistence/entity/meedlportfolio/DemographyEntity.java
@@ -19,32 +19,32 @@ public class DemographyEntity {
     @UuidGenerator
     private String id;
     private String name;
-    private int maleCount;
-    private int femaleCount;
-    private int totalGenderCount;
+    private int maleCount = 0;
+    private int femaleCount =  0;
+    private int totalGenderCount = 0;
     @Column(name = "age_17_to_25_count")
-    private int age17To25Count;
+    private int age17To25Count = 0;
     @Column(name = "age_25_to_35_count")
-    private int age25To35Count;
+    private int age25To35Count = 0;
     @Column(name = "age_35_to_45_count")
-    private int age35To45Count;
+    private int age35To45Count = 0;
     @Column(name = "south_east_count")
-    private int southEastCount;
+    private int southEastCount = 0;
     @Column(name = "south_west_count")
-    private int southWestCount;
+    private int southWestCount = 0;
     @Column(name = "south_south_count")
-    private int southSouthCount;
+    private int southSouthCount = 0;
     @Column(name = "north_east_count")
-    private int northEastCount;
+    private int northEastCount = 0;
     @Column(name = "north_west_count")
-    private int northWestCount;
+    private int northWestCount = 0;
     @Column(name = "north_central_count")
-    private int northCentralCount;
+    private int northCentralCount = 0;
     @Column(name = "non_nigerian")
-    private int nonNigerian;
+    private int nonNigerian = 0;
     @Column(name = "o_level_count")
-    private int oLevelCount;
+    private int oLevelCount = 0;
     @Column(name = "tertiary_count")
-    private int tertiaryCount;
+    private int tertiaryCount = 0;
 
 }
